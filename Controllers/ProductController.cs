23e17ab3cfaa8edfc5301faaaf5e466cad604f39
jarmatys <jarekarmatys@gmail.com>
2022-01-28 16:29:31 +0100

    Lekcja #3

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Test_web_app.Models;
 
 namespace Test_web_app.Controllers
 {
@@ -10,6 +11,56 @@ namespace Test_web_app.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Product()
+        {
+            var product = new Product
+            {
+                Id = 1,
+                Category = "Samochód",
+                Description = "To jest super samochód",
+                Name = "BMW M5"
+            };
+
+            return View(product);
+        }
+
+        public IActionResult List()
+        {
+            var productList = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Category = "Samochód",
+                    Description = "To jest super samochód",
+                    Name = "BMW M5"
+                },
+                new Product {
+                    Id = 2,
+                    Category = "Napoje",
+                    Description = "To jest opis napoju",
+                    Name = "PEPSI"
+                },
+                new Product {
+                    Id = 3,
+                    Category = "Roślina",
+                    Description = "To jest extra roślina",
+                    Name = "Storczyk"
+                }
+            };
+
+            return View(productList);
+        }
+
+        public IActionResult Data()
+        {
+            ViewBag.Name = "Jarek";
+            ViewData["Surname"] = "Armatys";
+            TempData["SecondName"] = "Aleksander";
+
             return View();
         }
     }
