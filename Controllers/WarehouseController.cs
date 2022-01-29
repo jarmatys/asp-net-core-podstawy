14a23179d2a7e0c8f055e155d306a306e778063a
jarmatys <jarekarmatys@gmail.com>
2022-01-29 13:38:00 +0100

    Lekcja #4

---
+++ Controllers/WarehouseController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test_web_app.Models;
+
+namespace Test_web_app.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product body)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(body);
+            }
+
+            // logika do zapisu produktu
+
+            return RedirectToAction("Add");
+        }
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,18 @@
-﻿namespace Test_web_app.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test_web_app.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Pole nazwa jest wymagane!")]
         public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public string Category { get; set; }
     }
 }
